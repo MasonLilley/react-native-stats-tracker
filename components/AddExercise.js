38f1a258f5e93a99b3efbe162108129d5121ac59
masonlilley <mason.lilley@gmail.com>
2024-09-18 17:10:09 -0500

    add react-native-paper

--- components/AddExercise.js
+++ components/AddExercise.js
@@ -1,35 +1,36 @@
-import * as SQLite from 'expo-sqlite';
 import React, { useState } from 'react';
-import { View, StyleSheet, Text, TextInput, TouchableOpacity, Button } from 'react-native';
+import { View, StyleSheet, Text, TextInput, TouchableOpacity, Button, Alert } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import * as FileSystem from 'expo-file-system'
-import { Asset } from 'expo-asset';
+import { Menu, Divider, Provider } from 'react-native-paper';
+import * as SQLite from 'expo-sqlite';
 
-// to clear table w/o breaking table structure:
-// let result = await db.execAsync("DELETE FROM Sets");
-
-function AddExercise({ exercise, index }) {
+function AddExercise({ exercise, deleteExercise }) {
     const db = SQLite.useSQLiteContext();
     const placeholderColor = '#888';
     const [rows, setRows] = useState([{ reps: '', weight: '', notes: '', muscle: 'tricep' }]);
+    const [focusedInputIndex, setFocusedInputIndex] = useState(null); // Track focused input index
+    const [menuVisible, setMenuVisible] = useState(false); // State for menu visibility
 
-    //DATABASE STUFF
+    // Toggle dropdown menu visibility
+    const toggleMenu = () => setMenuVisible(!menuVisible);
+    const closeMenu = () => setMenuVisible(false);
+
+    // DATABASE STUFF
 
     async function addToDatabase(row) {
         const statement = await db.prepareAsync("INSERT INTO Sets (exercise_name, reps, weight, date, note, muscle) VALUES ($name, $reps, $weight, $date, $note, $muscle)");
-
         const sets = await db.getAllAsync("SELECT * FROM Sets");
-        console.log("sets before: ",sets);
+        console.log("sets before: ", sets);
 
         try {
-            let result = await statement.executeAsync({ 
+            await statement.executeAsync({ 
                 $name: 'test',
                 $reps: row.reps, 
                 $weight: row.weight, 
                 $date: '2024-09-08', 
                 $note: row.notes,
-                $muscle: row.muscle});
-            // console.log("result:",result);
+                $muscle: row.muscle
+            });
         } catch(e) {
             console.log(e);
         } finally {
@@ -37,19 +38,29 @@ function AddExercise({ exercise, index }) {
             const sets = await db.getAllAsync("SELECT * FROM Sets");
             console.log("finalized async, sets now: ", sets);
         }
-      }
-
-
-    //ROW STUFF
+    }
 
     const addRow = () => {
         setRows([...rows, { name: 'test', reps: '', weight: '', notes: '', muscle: 'tricep' }]);
+    };
 
+    const handleFocus = (index) => {
+        setFocusedInputIndex(index);
+    };
+
+    const handleBlur = () => {
+        setFocusedInputIndex(null);
     };
 
     const logRows = async () => {
         for (const row of rows) {
-            await addToDatabase(row);
+            if (row.reps === '') {
+                Alert.alert('Undefined Reps', 'One or more of your sets have undefined reps. Please fix or press dismiss to autofill reps.', [{ text: 'OK', style: 'cancel' }]);
+            } else if (row.weight === '') {
+                Alert.alert('Undefined Weight', 'One or more of your sets have undefined weight. Please fix or press dismiss to autofill weight.', [{ text: 'OK', style: 'cancel' }]);
+            } else {
+                await addToDatabase(row);
+            }
         }
     };
 
@@ -73,120 +84,163 @@ function AddExercise({ exercise, index }) {
         setRows(updatedRows);
     };
 
-
-
     return (
-        <View style={styles.exerciseBox}>
-            <View style={styles.topRow}>
-                <View>
-                    <Text style={styles.exerciseName}>{exercise[index + 1][1].name}</Text>
-                    <Text style={styles.exerciseNotes}>{exercise[index + 1][2].notes}</Text>
+        <Provider>
+            <View style={styles.exerciseBox}>
+                <View style={styles.topRow}>
+                    <View>
+                        <Text style={styles.exerciseName}>{exercise.name}</Text>
+                        <Text style={styles.exerciseNotes}>{exercise.notes}</Text>
+                    </View>
+
+                    <TouchableOpacity onPress={deleteExercise} style={styles.trashIcon}>
+                        <Ionicons name="trash-outline" size={25} color="red" />
+                    </TouchableOpacity>
                 </View>
 
-                <TouchableOpacity onPress={addRow} style={styles.trashIcon}>
-                    <Ionicons name="trash-outline" size={25} color="red" />
-                </TouchableOpacity>
+                {rows.map((row, rowIndex) => (
+                    <View key={rowIndex} style={[styles.inputRow, focusedInputIndex === rowIndex && styles.focusedInputRow]}>
+                        <View style={styles.inputBox}>
+                            <Text style={styles.placeholderText}>Reps</Text>
+                            <TextInput
+                                style={styles.input}
+                                value={row.reps}
+                                onChangeText={(input) => handleRepsChange(input, rowIndex)}
+                                onFocus={() => handleFocus(rowIndex)}
+                                onBlur={handleBlur}
+                                keyboardType="numeric"
+                            />
+                        </View>
+
+                        <View style={styles.inputBox}>
+                            <Text style={styles.placeholderText}>Weight</Text>
+                            <TextInput
+                                style={styles.input}
+                                value={row.weight}
+                                onChangeText={(input) => handleWeightChange(input, rowIndex)}
+                                onFocus={() => handleFocus(rowIndex)}
+                                onBlur={handleBlur}
+                                keyboardType="decimal-pad"
+                            />
+                        </View>
+
+                        <View style={styles.inputBox}>
+                            <Text style={styles.placeholderText}>Notes</Text>
+                            <TextInput
+                                style={styles.input}
+                                value={row.notes}
+                                onChangeText={(input) => handleNotesChange(input, rowIndex)}
+                                onFocus={() => handleFocus(rowIndex)}
+                                onBlur={handleBlur}
+                            />
+                        </View>
+                    </View>
+                ))}
+
+                <View style={styles.lastRow}>
+                    <TouchableOpacity onPress={addRow} style={styles.icon}>
+                        <Ionicons name="barbell-outline" size={25} color="lightblue" />
+                    </TouchableOpacity>
+
+                    {/* Dropdown menu */}
+                    <Menu
+                        visible={menuVisible}
+                        onDismiss={closeMenu}
+                        anchor={
+                            <TouchableOpacity onPress={toggleMenu} style={styles.icon}>
+                                <Ionicons name="ellipsis-horizontal" size={25} color="lightblue" />
+                            </TouchableOpacity>
+                        }>
+                        <Menu.Item onPress={() => {}} title="Edit" />
+                        <Menu.Item onPress={() => {}} title="Duplicate" />
+                        <Divider />
+                        <Menu.Item onPress={() => {}} title="Delete" />
+                    </Menu>
+                </View>
+
+                <Button title="Log Rows" onPress={logRows} color="blue" />
+                <Button title="DELETE SETS" onPress={async () => {
+                    let result = await db.execAsync("DELETE FROM Sets");
+                    console.log("Deleted Sets");
+                }} color="red" />
             </View>
-
-        {rows.map((row, rowIndex) => (
-            <View key={rowIndex} style={styles.inputRow}>
-            <Text style={styles.placeholderText}>Reps</Text>
-            <TextInput
-                style={styles.input}
-                value={row.reps}
-                onChangeText={(input) => handleRepsChange(input, rowIndex)}
-                keyboardType="numeric" />
-            <Text style={styles.placeholderText}>Weight</Text>
-            <TextInput
-                style={styles.input}
-                value={row.weight}
-                onChangeText={(input) => handleWeightChange(input, rowIndex)}
-                keyboardType="decimal-pad" />
-            <Text style={styles.placeholderText}>Notes</Text>
-            <TextInput
-                style={styles.input}
-                value={row.notes}
-                onChangeText={(input) => handleNotesChange(input, rowIndex)} />
-            </View>
-        ))}
-
-        <View style={styles.lastRow}>
-            <TouchableOpacity onPress={addRow} style={styles.icon}>
-                <Ionicons name="barbell-outline" size={30} color="blue" />
-            </TouchableOpacity>
-        </View>
-
-        <Button title="Log Rows" onPress={logRows} color="blue" />
-        <Button title="DELETE SETS" onPress={async () => {
-            let result = await db.execAsync("DELETE FROM Sets");
-            console.log("Deleted Sets");
-            }} color="red"/>
-        </View>
+        </Provider>
     );
 }
 
-//
-// borderColor: 'red',
-// borderWidth: 1,
-//
 const styles = StyleSheet.create({
     exerciseBox: {
-    backgroundColor: '#1e1e1e',
-    borderRadius: 12,
-    padding: 16,
-    marginVertical: 8,
-    width: '90%',
-    alignSelf: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.2,
-    shadowRadius: 8,
-    elevation: 4,
+        backgroundColor: '#1e1e1e',
+        borderRadius: 12,
+        padding: 16,
+        marginVertical: 8,
+        width: '90%',
+        alignSelf: 'center',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.2,
+        shadowRadius: 8,
+        elevation: 4,
     },
     exerciseName: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#f0f0f0',
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#f0f0f0',
     },
     exerciseNotes: {
-    fontSize: 14,
-    color: '#b0b0b0',
-    marginBottom: 10,
+        fontSize: 14,
+        color: '#b0b0b0',
+        marginBottom: 10,
     },
     inputRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '90%',
-    marginBottom: 12,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        marginBottom: 12,
+        padding: 10,
+        paddingTop: 5,
+        borderWidth: 2,
+        borderRadius: 10,
+        borderColor: '#2c2c2c', 
+        backgroundColor: '#1c1c1e',
+    },
+    focusedInputRow: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.8,
+        shadowRadius: 8,
+        elevation: 6,
     },
     input: {
-    backgroundColor: '#333',
-    borderWidth: 1,
-    borderColor: '#444',
-    borderRadius: 8,
-    padding: 10,
-    width: '20%',
-    textAlign: 'center',
-    color: '#ffffff',
+        backgroundColor: '#333',
+        borderWidth: 1,
+        borderColor: '#444',
+        borderRadius: 8,
+        marginTop: 3,
+        padding: 8,
+        width: '70%',
+        textAlign: 'center',
+        color: '#ffffff',
     },
     icon: {
-    marginLeft: 0,
-    width: 35,
-    tintColor: '#f0f0f0',
+        marginLeft: 0,
+        width: 35,
+        tintColor: '#f0f0f0',
     },
     lastRow: {
-    borderColor: '#444',
-    borderWidth: 1,
-    width: '101%',
-    backgroundColor: '#2e2e2e',
-    borderRadius: 12,
-    padding: 12,
+        borderColor: '#444',
+        borderWidth: 1,
+        width: '100%',
+        height: 50,
+        backgroundColor: '#2e2e2e',
+        borderRadius: 12,
+        padding: 12,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
     placeholderText: {
         color: '#888',
-        paddingLeft: 7,
-        paddingRight: 10,
     },
     topRow: {
         flexDirection: 'row',
@@ -195,8 +249,12 @@ const styles = StyleSheet.create({
     trashIcon: {
         margin: 0,
         padding: 0,
+    },
+    inputBox: {
+        width: '30%',
+        alignItems: 'center',
+        justifyContent: 'center',
     }
 });
-
 
 export default AddExercise;
--- screens/CreateWorkoutModal.js
+++ screens/CreateWorkoutModal.js
@@ -1,8 +1,7 @@
 import { useRoute } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Button, Text, TouchableOpacity, ScrollView, TextInput, Touchable } from 'react-native';
+import { StyleSheet, View, Button, ScrollView, Alert } from 'react-native';
 import AddExercise from '../components/AddExercise';
-
 
 function CreateWorkoutModal({ navigation }) {
   const route = useRoute();
@@ -12,36 +11,55 @@ function CreateWorkoutModal({ navigation }) {
     console.log(route.params?.selectedExercise);
     if (route.params?.selectedExercise) {
       const newExercise = {
-        [exercises.length+1]: [
-          { muscle: route.params.selectedExercise.muscle },
-          { name: route.params.selectedExercise.name },
-          { notes: route.params.selectedExercise.notes },
-        ],
+        id: exercises.length + 1,
+        muscle: route.params.selectedExercise.muscle,
+        name: route.params.selectedExercise.name,
+        notes: route.params.selectedExercise.notes,
       };
       setExercises([...exercises, newExercise]);
     }
   }, [route.params?.selectedExercise]);
 
+  const deleteExercise = (id) => {
+    Alert.alert('Delete Exercise?', 'Are you sure you want to delete this exercise? This cannot be recovered.', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('hello'),
+        style: 'cancel'
+      },
+      {
+        text: 'Delete',
+        onPress: () => {setExercises(exercises.filter((exercise) => exercise.id !== id));},
+        style: 'destructive',
+      },
+
+    ]);    
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView style={styles.buttonContainer}>
         {exercises.map((exercise, index) => (
-          <AddExercise key={index} exercise={exercise} index={index} />
+          <AddExercise
+            key={exercise.id}
+            exercise={exercise}
+            index={index}
+            deleteExercise={() => deleteExercise(exercise.id)}
+          />
         ))}
 
-        <Button title="Add Dummy Exercise" onPress={() => {
-          const newIndex = exercises.length + 1;
-          setExercises([
-            ...exercises,
-            {
-              [newIndex]: [
-                { muscle: 'bicep' },
-                { name: 'Dumbbell Curl' },
-                { notes: 'Control eccentric' },
-              ],
-            },
-          ]);
-        }} />
+        <Button
+          title="Add Dummy Exercise"
+          onPress={() => {
+            const newExercise = {
+              id: exercises.length + 1,
+              muscle: 'bicep',
+              name: 'Dumbbell Curl',
+              notes: 'Control eccentric',
+            };
+            setExercises([...exercises, newExercise]);
+          }}
+        />
       </ScrollView>
     </View>
   );
@@ -53,6 +71,7 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   buttonContainer: {
+    padding: 10,
   },
 });
 
